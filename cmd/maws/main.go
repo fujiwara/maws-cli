99a4f80c52cdafea4f8a74177d372f06653a4e33
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-05-29 00:44:23 +0900

    fix version

--- cmd/maws/main.go
+++ cmd/maws/main.go
@@ -3,18 +3,29 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/fujiwara/maws-cli"
 )
 
+var version string
+
 func main() {
+	var showVersion bool
 	opt := maws.Option{}
 	flag.StringVar(&opt.Config, "config", "maws.yaml", "path of a config file")
 	flag.BoolVar(&opt.BufferStdout, "buffering", true, "buffering stdout of aws cli")
 	flag.Int64Var(&opt.MaxParallels, "max-parallels", 10, "max parallels")
+	flag.BoolVar(&showVersion, "version", false, "show version")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Println("maws version", version)
+		return
+	}
+
 	opt.Commands = flag.Args()
 	errCount, err := maws.Run(context.Background(), opt)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.6.0
 	github.com/aws/aws-sdk-go-v2/config v1.3.0
 	github.com/aws/aws-sdk-go-v2/service/sts v1.4.1
-	github.com/kayac/go-config v0.5.1 // indirect
+	github.com/kayac/go-config v0.5.1
 	github.com/pkg/errors v0.9.1
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
